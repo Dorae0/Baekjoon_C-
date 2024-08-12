65262f45ccaf2ee3d8ce3e0385aa46fc759f4aeb
Dorae0 <126739831+Dorae0@users.noreply.github.com>
2024-08-12 21:52:43 +0900

    [Silver III] Title: N과 M (8), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+#define MAX 8
+using namespace std;
+
+int n, m;
+int arr[MAX], backtracking_arr[MAX];
+bool visited[MAX];
+
+void backtracking(int cnt, int currentNum);
+
+int main()
+{
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
+	sort(arr, arr + n);
+
+	backtracking(0, 0);
+
+	return 0;
+}
+
+void backtracking(int cnt, int currentNum)
+{
+	if (cnt == m)
+	{
+		for (int i = 0; i < m; i++)
+			//cout << backtracking_arr[i] << ' ';
+			printf("%d ", backtracking_arr[i]);
+		//cout << endl;
+		printf("\n");
+		return;
+	}
+	for (int i = currentNum; i < n; i++)
+	{
+		backtracking_arr[cnt] = arr[i];
+		backtracking(cnt + 1, i);
+	}
+}
