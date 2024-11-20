bce7413f50d55b346414d4daa4beece285a6c456
Dorae0 <126739831+Dorae0@users.noreply.github.com>
2024-11-20 09:21:47 +0900

    [Gold V] Title: 치킨 배달, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.cc
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.cc
@@ -1,23 +1,21 @@
 #include <iostream>
 #include <vector>
 
-#define INF ~0U >> 2
-
 using namespace std;
 int n, m, temp;
-int ans = INF;
+int ans = ~0U >> 2;
 bool visited[13];
-vector<pair<int, int>> home;
+vector<pair<int, int>> house;
 vector<pair<int, int>> chicken;
 
 void Init();
-void FindChickenDistance(int Idx, int Count);
-int CheckChickenDistance();
+void Backtracking(int CurrentIdx, int Count);
+int CalculateDistance();
 
 int main()
 {
     Init();
-    FindChickenDistance(0, 0);
+    Backtracking(0, 0);
     cout << ans;
 }
 
@@ -31,48 +29,42 @@ void Init()
         {
             cin >> temp;
             if(temp == 1)
-                home.push_back({j, i});
+                house.push_back({j, i});
             else if(temp == 2)
                 chicken.push_back({j, i});
         }
 }
 
-void FindChickenDistance(int Idx, int Count)
+void Backtracking(int CurrentIdx, int Count)
 {
     if(Count == m)
     {
-        ans = min(ans,CheckChickenDistance());
+        ans = min(ans, CalculateDistance());
         return;
     }
-    for(int i = Idx; i < chicken.size(); i++)
+    for(int i = CurrentIdx; i < chicken.size(); i++)
     {
-        int NextX = chicken[i].first;
-        int NextY = chicken[i].second;
         if(!visited[i])
         {
             visited[i] = true;
-            FindChickenDistance(i + 1, Count + 1);
+            Backtracking(i + 1, Count + 1);
             visited[i] = false;
         }
     }
 }
 
-int CheckChickenDistance()
+int CalculateDistance()
 {
-    int ChickenAns = 0;
-    for(int i = 0; i < home.size(); i++)
+    int ans = 0;
+    for(int i = 0; i < house.size(); i++)
     {
-        int HomeX = home[i].first;
-        int HomeY = home[i].second;
-        int CurrentDist = INF;
+        int CurrentDistance = ~0U >> 2;
         for(int j = 0; j < chicken.size(); j++)
         {
-            int ChickenX = chicken[j].first;
-            int ChickenY = chicken[j].second;
             if(visited[j])
-                CurrentDist = min(CurrentDist, abs(HomeX - ChickenX) + abs(HomeY - ChickenY));
+                CurrentDistance = min(CurrentDistance, abs(house[i].first - chicken[j].first) + abs(house[i].second - chicken[j].second));
         }
-        ChickenAns += CurrentDist;
+        ans += CurrentDistance;
     }
-    return ChickenAns;
+    return ans;
 }
