4f1fd2a549543c312c0c87d324ead10f379d47ee
Dorae0 <126739831+Dorae0@users.noreply.github.com>
2024-11-22 17:59:38 +0900

    [Gold IV] Title: 사전 순 최대 공통 부분 수열, Time: 0 ms, Memory: 2064 KB -BaekjoonHub

---
+++ 백준/Gold/30805. 사전 순 최대 공통 부분 수열/사전 순 최대 공통 부분 수열.cc
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#define MAX 101
+
+using namespace std;
+
+struct compare
+{
+	bool operator()(pair<int, int> a, pair<int, int> b)
+	{
+		if(a.first == b.first)
+			return a.second > b.second;
+		return a.first < b.first;
+	}
+};
+
+int n, m;
+vector<int> ans;
+priority_queue<pair<int, int>, vector<pair<int, int>>, compare> pq_a;
+priority_queue<pair<int, int>, vector<pair<int, int>>, compare> pq_b;
+vector<pair<int, int>> temp;
+int a[MAX];
+int b[MAX];
+
+int dp[MAX][MAX];
+
+void Init();
+void Latest_Dict();
+
+int main()
+{
+    Init();
+    Latest_Dict();
+	cout << ans.size() << '\n';
+	for(int i = 0; i < ans.size(); i++)
+		cout << ans[i] << ' ';
+}
+
+void Init()
+{
+    cin.tie(0); cout.tie(0);
+    ios::sync_with_stdio(false);
+    cin >> n;
+    for(int i = 0; i < n; i++)
+	{
+		cin >> a[i];
+		pq_a.push({a[i], i});
+	}
+    cin >> m;
+    for(int i = 0; i < m; i++)
+    {
+		cin >> b[i];
+		pq_b.push({b[i], i});
+	}
+}
+
+void Latest_Dict()
+{
+	int Idx_A = 0;
+	int Idx_B = 0;
+
+	while(!pq_a.empty())
+	{
+		int CurrentVal = pq_a.top().first;
+		int CurrentIdx = pq_a.top().second;
+		pq_a.pop();
+		if(CurrentIdx < Idx_A)
+			continue;
+		while(!pq_b.empty())
+		{
+			if(pq_b.top().second < Idx_B)
+			{
+				pq_b.pop();
+				continue;
+			}
+			if(CurrentVal == pq_b.top().first)
+			{
+				Idx_A = CurrentIdx;
+				Idx_B = pq_b.top().second;
+				ans.push_back(CurrentVal);
+				pq_b.pop();
+				break;
+			}
+			else if(CurrentVal < pq_b.top().first)
+			{
+				temp.push_back(pq_b.top());
+				pq_b.pop();
+			}
+			else
+			{
+				for(int i = 0; i < temp.size(); i++)
+				{
+					pq_b.push(temp.back());
+					temp.pop_back();
+				}
+				break;
+			}
+		}
+	}
+}
