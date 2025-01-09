8950ab726fe5796bdc2508e1eaba729f58719798
Dorae0 <126739831+Dorae0@users.noreply.github.com>
2025-01-09 17:12:36 +0900

    [Platinum II] Title: N-Queen 2, Time: 0 ms, Memory: 2020 KB, Score: 405 point -BaekjoonHub

--- 백준/Platinum/21133. N－Queen 2/N－Queen 2.cc
+++ 백준/Platinum/21133. N－Queen 2/N－Queen 2.cc
@@ -21,8 +21,29 @@ void init()
 
 void nQueen()
 {
-	for (int i = n / 2; i > 0; i--)
-		cout << i << '\n' << i + n / 2 << '\n';
-	if (n % 2 == 1)
-		cout << n;
+	if (n % 6 == 2)
+	{
+		for (int i = 2; i <= n; i += 2)
+			cout << i << '\n';
+		cout << 3 << '\n' << 1 << '\n';
+		for (int i = 7; i <= n; i += 2)
+			cout << i << '\n';
+		cout << 2;
+	}
+	else if (n % 6 == 3)
+	{
+		for (int i = 4; i <= n; i += 2)
+			cout << i << '\n';
+		cout << 2 << '\n';
+		for (int i = 5; i <= n; i += 2)
+			cout << i << '\n';
+		cout << 1 << '\n' << 3 << '\n';
+	}
+	else
+	{
+		for (int i = n / 2; i > 0; i--)
+			cout << i << '\n' << i + n / 2 << '\n';
+		if (n % 2 == 1)
+			cout << n;
+	}
 }
